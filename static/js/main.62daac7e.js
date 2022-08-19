a8528a6441c38c083832b593551a99fb4bfe1d33
Rajat-2701 <rajatchugh7898@gmail.com>
2022-08-19 18:28:43 +0530

    helklo

--- static/js/main.62daac7e.js
+++ static/js/main.62daac7e.js
@@ -18817,7 +18817,7 @@
                         muted: !0,
                       }),
                       (0, pe.jsx)("video", {
-                        src: Rr,
+                        src: mediaBg,
                         className:
                           "w-full h-auto fixed top-0 lg:hidden md:hidden sm:block",
                         width: "100",
